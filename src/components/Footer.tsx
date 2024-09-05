9ce1901817f58bb38ea3f239c395b8df672a834d
Gaurav Prasad <100416827+gauravver13@users.noreply.github.com>
2024-09-05 19:22:54 +0530

    added component function

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-yellow-100'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='text-5xl bg-red-600 min-w-full m-0 p-0'>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar
